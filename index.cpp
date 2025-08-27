e9080c83d1f981b08dec1ca41ac57e3587b615be
Osier Nono <osiernono@gmail.com>
2025-08-27 15:04:26 -0500

    having a libarary names strextra for find and testing it

--- index.cpp
+++ index.cpp
@@ -1,31 +1,11 @@
 #include <iostream>
 #include <cassert>
 #include <string>
+#include "strextra.cpp"
 
 using namespace std;
 
-int find(string str, char c)
-{
-    for(int i=0; i< str.length(); i++)
-    {
-        if(str[i] == c)
-        {
-            return i;
-        }
-    }
-    return -1;
-}
 
-int find(string str, string c)
-{
-    size_t index= str.find(c);
-
-    if (str.find(c))
-    {
-        return index;
-    }
-    return -1;
-}
 
 int main()
 {
@@ -33,10 +13,15 @@ int main()
     cout << find("game",'a') << endl;
     cout << find ("game", 'e')<< endl;
     cout << find("game", 'r') <<endl;
-    //testing with words
 
+    //testing with words
     cout << find("the quick brown fox jumps over the lazy dog", "fox") << endl;
     cout << find("The brown apple", "brown") << endl;
     cout << find("Im a star and a diva", "fox") << endl;
 
+    //trying it asserts
+    assert(find("where are you", 'a')==6);
+    assert(find("come here jesan","jesan")==10);
+    assert(find("done",'v')==-1);
+    assert(find("basket of apples","jesan")==-1);
 }
---
+++ strextra.cpp
@@ -0,0 +1,26 @@
+#include <string>
+
+using namespace std;
+
+int find(string str, char c)
+{
+    for(int i=0; i< str.length(); i++)
+    {
+        if(str[i] == c)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int find(string str, string c)
+{
+    size_t index= str.find(c);
+
+    if (str.find(c))
+    {
+        return index;
+    }
+    return -1;
+}
