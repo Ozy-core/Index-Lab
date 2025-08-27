4feed5233c1f58cc51175879dfb439346bcead00
Osier Nono <osiernono@gmail.com>
2025-08-27 14:51:38 -0500

    testing find with two character constructors

--- index.cpp
+++ index.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <cassert>
 #include <string>
+
 using namespace std;
 
-int looking_for(string str, char c)
+int find(string str, char c)
 {
     for(int i=0; i< str.length(); i++)
     {
@@ -15,9 +16,27 @@ int looking_for(string str, char c)
     return -1;
 }
 
+int find(string str, string c)
+{
+    size_t index= str.find(c);
+
+    if (str.find(c))
+    {
+        return index;
+    }
+    return -1;
+}
+
 int main()
 {
-    cout << looking_for("game",'a') << endl;
-    cout << looking_for ("game", 'e')<< endl;
-    cout << looking_for("game", 'r') <<endl;
+    // test: looking for characters and not strings
+    cout << find("game",'a') << endl;
+    cout << find ("game", 'e')<< endl;
+    cout << find("game", 'r') <<endl;
+    //testing with words
+
+    cout << find("the quick brown fox jumps over the lazy dog", "fox") << endl;
+    cout << find("The brown apple", "brown") << endl;
+    cout << find("Im a star and a diva", "fox") << endl;
+
 }
