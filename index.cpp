0a9e7209bb76d25b7a780be609abe643e9f3dc82
Osier Nono <osiernono@gmail.com>
2025-08-27 11:39:12 -0500

    adding code to looking_for

--- index.cpp
+++ index.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <cassert>
+#include <string>
+using namespace std;
+
+int looking_for(string str, char c)
+{
+    for(int i=0; i< str.length(); i++)
+    {
+        if(str[i] == c)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
 
 int main()
 {
-    
+
+    return 0;
 }
